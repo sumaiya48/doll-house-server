124a2440bdb0fc4a2293add4116c2ce36e4dafb0
sumaiya48 <sumaiyaislam202021@gmail.com>
2024-05-30 21:35:52 +0600

    deployDone

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server
-    await client.connect();
+    // await client.connect();
 
     const dollCollection = client.db('dollUser').collection('doll');
 
